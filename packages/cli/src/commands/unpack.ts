e38887041c87eb05811c1723caf96328c9e5657e
Santiago Palladino <spalladino@gmail.com>
2019-04-26 22:45:20 +0300

    Update packages/cli/src/commands/unpack.ts
    Co-Authored-By: ylv-io <to@ylv.io>

--- packages/cli/src/commands/unpack.ts
+++ packages/cli/src/commands/unpack.ts
@@ -2,7 +2,7 @@ import unpack from '../scripts/unpack';
 
 const name: string = 'unpack';
 const signature: string = `${name} [kit]`;
-const description: string = `Download and install ZeppelinOS kit to the current working directory`;
+const description: string = `Download and install a ZeppelinOS kit to the current directory`;
 
 const register: (program: any) => any = (program) => program
   .command(signature, undefined, { noHelp: true })
